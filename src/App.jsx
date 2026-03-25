a5187efb7493422b32d049911b846851c1b38f38
iamstivgo <iamstivgo@gmail.com>
2026-03-24 20:37:46 -0500

    feat: add additional information about software engineering in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <h1>¡Hola DevOps! 🚀</h1>
       <p>Esta app fue desplegada automáticamente con GitHub Actions</p>
+      <p>Ingeniería de Software</p>
     </div>
   )
 }
